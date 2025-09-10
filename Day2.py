399c278ddd1767dfda332890e00c7cd33650fe40
Rahul Singh <rahulsingh51775177@gmail.com>
2025-09-10 21:25:07 +0530

    Update Day2.py

--- Day2.py
+++ Day2.py
@@ -5,10 +5,8 @@ nameOfPerson = "Aranha"
 
 # Storing an age in a variable
 age = 42
-
 # String
 name = "Vivian"
-
 # Integer
 age = 42
 
@@ -49,4 +47,5 @@ print("\n---- Personalized Greeting ----")
 print(f"Hello, {name}! 👋")
 print(f"You are {age} years old and {color} is a beautiful color!")
 print("You're now ready to start your Python adventure 🚀🐍")
+
 
