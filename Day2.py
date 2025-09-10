65a9b65ef0360bd4143c16f13f5f57ca42e0fb12
Rahul Singh <rahulsingh51775177@gmail.com>
2025-09-10 21:25:55 +0530

    Update Day2.py

--- Day2.py
+++ Day2.py
@@ -1,9 +1,11 @@
 # Variables and Data Types in Python
 # Storing a name in a variable
+
 name = "Vivian"
 nameOfPerson = "Aranha"
 
 # Storing an age in a variable
+
 age = 42
 # String
 name = "Vivian"
@@ -49,6 +51,7 @@ print("\n---- Personalized Greeting ----")
 print(f"Hello, {name}! 👋")
 print(f"You are {age} years old and {color} is a beautiful color!")
 print("You're now ready to start your Python adventure 🚀🐍")
+
 
 
 
