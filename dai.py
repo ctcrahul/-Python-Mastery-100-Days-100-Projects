99878805fa4e95e527fb7d002301c75781d4ac7d
Rahul Singh <rahulsingh51775177@gmail.com>
2025-09-18 17:13:19 +0530

    Update dai.py

--- dai.py
+++ dai.py
@@ -1,7 +1,7 @@
 ﻿# Python Mastery Projects
 print("Hlo World")
 
-
+# MINI PROJECTS
 
 # To DO List
 todo = []
@@ -10,3 +10,12 @@ todo.append("Study Python")
 print(todo)
 
 
+# Number Guessing Game 
+
+import random
+num = random.randint(1, 10)
+guess = int(input("Guess a number (1-10): "))
+if guess == num:
+    print("Correct!")
+else:
+    print("Wrong, number was", num)
