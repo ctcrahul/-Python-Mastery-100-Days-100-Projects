5df0fe02815293c06abc5050a3eadd751467177b
Rahul Singh <rahulsingh51775177@gmail.com>
2025-09-21 18:20:34 +0530

    Update day4.py

--- day4.py
+++ day4.py
@@ -111,3 +111,10 @@ import copy
 copy4 = copy.deepcopy(original)
 
 
+
+    original[2].append(5)
+print(copy1)
+# [1, 2, [3, 4, 5]] — nested list changed!
+print(copy4)
+# [1, 2, [3, 4]] — unchanged
+
