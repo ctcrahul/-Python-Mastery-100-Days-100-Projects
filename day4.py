5682c9ed0c00ee434d197e6251fc0c7ba3f665dc
Rahul Singh <rahulsingh51775177@gmail.com>
2025-09-21 18:19:53 +0530

    Update day4.py

--- day4.py
+++ day4.py
@@ -93,3 +93,21 @@ print(copy1)
 print(copy4)
 # [1, 2, [3, 4]] — unchanged
 
+
+
+    original = [1, 2, [3, 4]]
+
+# 1. Slice (shallow copy)
+copy1 = original[:]
+
+# 2. .copy() method (shallow copy)
+copy2 = original.copy()
+
+# 3. Using list() (shallow copy)
+copy3 = list(original)
+
+# 4. deepcopy (deep copy)
+import copy
+copy4 = copy.deepcopy(original)
+
+
