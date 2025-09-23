94b5a58fda9322ee717354b6e0d906e932a22ea7
Rahul Singh <rahulsingh51775177@gmail.com>
2025-09-23 21:03:35 +0530

    Update day5.py

--- day5.py
+++ day5.py
@@ -79,3 +79,32 @@ def remove_duplicates(lst):
 
 my_list = [1, 2, 2, 3, 4, 4, 5]
 print(remove_duplicates(my_list))  # [1, 2, 3, 4, 5]
+
+
+9. How to reverse a linked list in Python?
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+def reverse_linked_list(head):
+    prev = None
+    current = head
+    while current:
+        next_node = current.next
+        current.next = prev
+        prev = current
+        current = next_node
+    return prev
+
+# Create linked list: 1 -> 2 -> 3
+head = Node(1)
+head.next = Node(2)
+head.next.next = Node(3)
+
+# Reverse and print the list
+reversed_head = reverse_linked_list(head)
+while reversed_head:
+    print(reversed_head.data, end=" -> ")
+    reversed_head = reversed_head.next
