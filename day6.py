719874d26e141a9769a41d259f0b554a4b3da6d9
Rahul Singh <rahulsingh51775177@gmail.com>
2025-09-24 20:57:32 +0530

    Update day6.py

--- day6.py
+++ day6.py
@@ -68,9 +68,5 @@ def math_quiz():
 
     if user_answer == correct_answer:
       print("Correct!")
-      score += 1
-    else:
-      print(f"Wrong! The correct answer is: {correct_answer}")
-
-
+ 
 
