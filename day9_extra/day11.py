ee1ed2d039e725de625968631c4e88e70d4b42f8
Rahul Singh <rahulsingh51775177@gmail.com>
2025-09-29 21:16:34 +0530

    Update day11.py

--- day9_extra/day11.py
+++ day9_extra/day11.py
@@ -88,7 +88,5 @@ while True:
     num1 = float(input("Enter first number: "))
     num2 = float(input("Enter second number: "))
 
-    if choice == '1':
-      print("Result:", add(num1, num2))
-    elif choice == '2':
+
 
