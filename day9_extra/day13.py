f92f0da80fc0f37ed90d1e4bd5182b006d9417c9
Rahul Singh <rahulsingh51775177@gmail.com>
2025-10-01 17:50:41 +0530

    Update day13.py

--- day9_extra/day13.py
+++ day9_extra/day13.py
@@ -10,11 +10,3 @@
 squares = [x**2 for x in range(10)]
 print(squares)
 
-numbers = [1, 2, 3, 4, 5]
-doubled = [x * 2 for x in numbers]
-print(doubled)
-
-numbers = [1, 2, 3, 4, 5, 6, 7]
-evens = [x for x in numbers if x % 2 == 0]
-print(evens)
-
