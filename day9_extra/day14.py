ea2488551fecf517e64b3261bf3130ccc67e63b8
Rahul Singh <rahulsingh51775177@gmail.com>
2025-10-02 18:03:12 +0530

    Update day14.py

--- day9_extra/day14.py
+++ day9_extra/day14.py
@@ -73,8 +73,7 @@ try:
   length = int(input("Enter the desired password length (minimum 4): "))
   password = generate_password(length)
   print("Generated Password:", password)
-except ValueError as e:
-  print(e)
+
 
 
 
