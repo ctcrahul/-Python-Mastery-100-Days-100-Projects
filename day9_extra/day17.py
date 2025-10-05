f224b74b986b8fc73214048ce3aaf71c732d0df9
Rahul Singh <rahulsingh51775177@gmail.com>
2025-10-05 19:53:04 +0530

    Update day17.py

--- day9_extra/day17.py
+++ day9_extra/day17.py
@@ -10,12 +10,7 @@ import csv
 with open('students.csv', 'r') as file:
   reader = csv.reader(file)
   for row in reader:
-    print(row)
-
-import csv
-
-with open('students.csv', 'r') as file:
-  reader = csv.DictReader(file)
+ 
   for row in reader:
     print(row)
 
