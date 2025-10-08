afeec6acc6eedb2bff72ff6a0a5aae3bf479f1db
Rahul Singh <rahulsingh51775177@gmail.com>
2025-10-08 17:13:08 +0530

    Update day20.py

--- day9_extra/day20.py
+++ day9_extra/day20.py
@@ -12,12 +12,6 @@
 from datetime import datetime, timedelta
 import time
 
-# Step 1: Get Event Date and Time from User
-def get_event_datetime():se YYYY-MM-DD HH:MM:SS format.")
-    return None
-
-    time_left = calculate_time_remaining(event_date)
-    if time_left.total_seconds() <= 0:
       print("\nCountdown Complete!")
       break
     display_countdown(time_left)
