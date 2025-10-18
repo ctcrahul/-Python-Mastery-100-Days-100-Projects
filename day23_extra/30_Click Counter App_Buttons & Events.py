b148eee6a9101b44be2acc36ea5fa5bef192eea5
Rahul Singh <rahulsingh51775177@gmail.com>
2025-10-18 20:02:52 +0530

    Update 30_Click Counter App_Buttons & Events.py

--- day23_extra/30_Click Counter App_Buttons & Events.py
+++ day23_extra/30_Click Counter App_Buttons & Events.py
@@ -17,11 +17,7 @@ reset_button.pack(pady=10)
 
 # Exit Button
 exit_button = tk.Button(root, text="Exit", command=root.destroy, font=("Arial", 14), bg="#607d8b", fg="black")
-exit_button.pack(pady=20)
-
-# Run the App
-root.mainloop()
-
+exit_button.pack(pady=20)opop
 
 
 
