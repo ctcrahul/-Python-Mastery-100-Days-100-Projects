c00f771ecf0284f7beeb679221d01a4c452b464d
Rahul Singh <rahulsingh51775177@gmail.com>
2025-10-18 20:03:21 +0530

    Update 30_Click Counter App_Buttons & Events.py

--- day23_extra/30_Click Counter App_Buttons & Events.py
+++ day23_extra/30_Click Counter App_Buttons & Events.py
@@ -11,10 +11,7 @@ import tkinter as tk
 root = tk.Tk().lll
 # Reset Functionl,l
 # Counter Label
-counter_label = tk.Label(root, text="Clicks: 0", font=.,.,=increment, font=("Arial", 14), bg="#4caf50", fg="black")
-increment_button.pack(pady=10)ll4), bg="#f44336", fg="black")
-reset_button.pack(pady=10)
-
+counter_label = tk.Label(root, text="Clicks: llll
 # Exit Button
 exit_button = tk.Button(root, text="Exit", command=root.destroy, font=("Arial", 14), bg="#607d8b", fg="black")
 exit_button.pack(pady=20)opop
