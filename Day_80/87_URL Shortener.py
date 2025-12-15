c3491960891cc3cb8b24185bf8c33ad7bba214d1
Rahul Singh <rahulsingh51775177@gmail.com>
2025-12-15 18:40:12 +0530

    Update 87_URL Shortener.py

--- Day_80/87_URL Shortener.py
+++ Day_80/87_URL Shortener.py
@@ -27,3 +27,31 @@ def init_db():
                 clicks INTEGER DEFAULT 0
             )
         """)
+
+def generate_code(length=6):
+    return "".join(random.choice(BASE62) for _ in range(length))
+
+
+def save_url(code, url):
+    with sqlite3.connect(DB) as conn:
+        conn.execute(
+            "INSERT INTO urls (code, url) VALUES (?, ?)",
+            (code, url)
+        )
+
+
+def get_url(code):
+    with sqlite3.connect(DB) as conn:
+        cur = conn.execute(
+            "SELECT url, clicks FROM urls WHERE code=?",
+            (code,)
+        )
+        return cur.fetchone()
+
+
+def increment_click(code):
+    with sqlite3.connect(DB) as conn:
+        conn.execute(
+            "UPDATE urls SET clicks = clicks + 1 WHERE code=?",
+            (code,)
+        )
