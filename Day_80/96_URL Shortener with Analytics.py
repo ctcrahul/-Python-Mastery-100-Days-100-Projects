3519a82270dda661f34b5a81f29e39ac8ef38d7c
Rahul Singh <rahulsingh51775177@gmail.com>
2025-12-24 20:58:48 +0530

    Update 96_URL Shortener with Analytics.py

--- Day_80/96_URL Shortener with Analytics.py
+++ Day_80/96_URL Shortener with Analytics.py
@@ -21,3 +21,28 @@ def load_data():
 def save_data(data):
     with open(DATA_FILE, "w") as f:
         json.dump(data, f, indent=4)
+# -----------------------------
+# Generate Short Code
+# -----------------------------
+def generate_code(length=6):
+    chars = string.ascii_letters + string.digits
+    return "".join(random.choice(chars) for _ in range(length))
+
+# -----------------------------
+# Shorten URL
+# -----------------------------
+def shorten_url(original_url):
+    data = load_data()
+    short_code = generate_code()
+
+    while short_code in data:
+        short_code = generate_code()
+
+    data[short_code] = {
+        "original_url": original_url,
+        "created_at": datetime.now().isoformat(),
+        "clicks": 0
+    }
+
+    save_data(data)
+    return short_code
