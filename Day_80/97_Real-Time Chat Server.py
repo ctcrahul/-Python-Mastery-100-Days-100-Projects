d0d087f5a2b7f9ad91970b5ca0b50b133c35d73c
Rahul Singh <rahulsingh51775177@gmail.com>
2025-12-25 18:55:50 +0530

    Update 97_Real-Time Chat Server.py

--- Day_80/97_Real-Time Chat Server.py
+++ Day_80/97_Real-Time Chat Server.py
@@ -1,5 +1,5 @@
 # Project 97: Real-Time Chat Server
-# Author: You
+##################################### #######  server.py  ##################################### #######      
 
 import socket
 import threading
@@ -52,3 +52,44 @@ def receive():
 
 if __name__ == "__main__":
     receive()
+
+
+
+###################################### client.py #################################################
+# Project 97: Chat Client
+# Author: You
+
+import socket
+import threading
+
+HOST = "127.0.0.1"
+PORT = 5555
+
+nickname = input("Choose your nickname: ")
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST, PORT))
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode("utf-8")
+            if message == "NICK":
+                client.send(nickname.encode("utf-8"))
+            else:
+                print(message)
+        except:
+            print("Disconnected from server.")
+            client.close()
+            break
+
+def write():
+    while True:
+        message = f"{nickname}: {input('')}"
+        client.send(message.encode("utf-8"))
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
