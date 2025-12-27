2e35c1355edb5c9e151cf8357404d6be9291d15a
Rahul Singh <rahulsingh51775177@gmail.com>
2025-12-27 19:49:53 +0530

    Update 99_rate_limiter.py

--- Day_80/99_rate_limiter.py
+++ Day_80/99_rate_limiter.py
@@ -28,3 +28,22 @@ if __name__ == "__main__":
         allowed = is_allowed(user)
         print(f"Request {i+1} ->", "ALLOWED" if allowed else "BLOCKED")
         time.sleep(1)
+
+
+
+= r.pipeline()
+    pipe.zremrangebyscore(key, 0, now - WINDOW)
+    pipe.zcard(key)
+    pipe.zadd(key, {now: now})
+    pipe.expire(key, WINDOW)
+    _, count, _, _ = pipe.execute()
+
+    return count < RATE_LIMIT
+
+
+# --------- Test Simulation ----------
+if __name__ == "__main__":
+    user = "user_123"
+
+    for i in range(10):
+        allowed = is_al
