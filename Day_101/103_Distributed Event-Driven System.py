4a9fd215f9149d3e8a73550ad85703d2d54d242e
Rahul Singh <rahulsingh51775177@gmail.com>
2025-12-31 21:11:16 +0530

    Implement EventBroker with publish method

--- Day_101/103_Distributed Event-Driven System.py
+++ Day_101/103_Distributed Event-Driven System.py
@@ -7,3 +7,17 @@ from collections import defaultdict, deque
 # BROKER CORE
 # -----------------------------
 class EventBroker:
+      def __init__(self):
+        self.topics = defaultdict(deque)
+        self.subscribers = defaultdict(list)
+        self.lock = threading.Lock()
+
+    def publish(self, topic, message):
+        with self.lock:
+            event = {
+                "id": str(uuid.uuid4()),
+                "timestamp": time.time(),
+                "data": message
+            }
+            self.topics[topic].append(event)
+            print(f"[BROKER] Event published to '{topic}'")
