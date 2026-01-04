7763f33a3d80bcb375735d004692fcc8ced9a578
Rahul Singh <rahulsingh51775177@gmail.com>
2026-01-04 19:42:51 +0530

    Update 107_Reinforcement Learning from Scratch.py

--- Day_101/107_Reinforcement Learning from Scratch.py
+++ Day_101/107_Reinforcement Learning from Scratch.py
@@ -14,3 +14,23 @@ ACTIONS = ["UP", "DOWN", "LEFT", "RIGHT"]
 # Q-TABLE
 # -----------------------------
 Q = {}
+def get_q(state, action):
+    return Q.get((state, action), 0.0)
+
+def set_q(state, action, value):
+    Q[(state, action)] = value
+
+# -----------------------------
+# ENV STEP
+# -----------------------------
+def step(state, action):
+    x, y = state
+
+    if action == "UP":
+        x -= 1
+    elif action == "DOWN":
+        x += 1
+    elif action == "LEFT":
+        y -= 1
+    elif action == "RIGHT":
+        y += 1
