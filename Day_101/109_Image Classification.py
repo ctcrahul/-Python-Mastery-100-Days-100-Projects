c79a70a0f871913937eb7e8b18f0a2f8dd1e5f2e
Rahul Singh <rahulsingh51775177@gmail.com>
2026-01-06 20:28:55 +0530

    Update 109_Image Classification.py

--- Day_101/109_Image Classification.py
+++ Day_101/109_Image Classification.py
@@ -12,3 +12,26 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 IMG_SIZE = 128
 BATCH_SIZE = 32
 EPOCHS = 10
+
+
+TRAIN_DIR = "dataset/train"
+TEST_DIR = "dataset/test"
+
+# -----------------------------
+# DATA GENERATORS
+# -----------------------------
+train_gen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=20,
+    zoom_range=0.2,
+    horizontal_flip=True
+)
+
+test_gen = ImageDataGenerator(rescale=1./255)
+
+train_data = train_gen.flow_from_directory(
+    TRAIN_DIR,
+    target_size=(IMG_SIZE, IMG_SIZE),
+    batch_size=BATCH_SIZE,
+    class_mode="binary"
+)
