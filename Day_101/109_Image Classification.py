f1485b62b29a7b57a031109d7aecb25f3b02b198
Rahul Singh <rahulsingh51775177@gmail.com>
2026-01-06 20:29:10 +0530

    Update 109_Image Classification.py

--- Day_101/109_Image Classification.py
+++ Day_101/109_Image Classification.py
@@ -35,3 +35,28 @@ train_data = train_gen.flow_from_directory(
     batch_size=BATCH_SIZE,
     class_mode="binary"
 )
+test_data = test_gen.flow_from_directory(
+    TEST_DIR,
+    target_size=(IMG_SIZE, IMG_SIZE),
+    batch_size=BATCH_SIZE,
+    class_mode="binary"
+)
+
+# -----------------------------
+# CNN MODEL
+# -----------------------------
+model = Sequential([
+    Conv2D(32, (3,3), activation="relu", input_shape=(IMG_SIZE, IMG_SIZE, 3)),
+    MaxPooling2D(2,2),
+
+    Conv2D(64, (3,3), activation="relu"),
+    MaxPooling2D(2,2),
+
+    Conv2D(128, (3,3), activation="relu"),
+    MaxPooling2D(2,2),
+
+    Flatten(),
+    Dense(128, activation="relu"),
+    Dropout(0.5),
+    Dense(1, activation="sigmoid")
+])
