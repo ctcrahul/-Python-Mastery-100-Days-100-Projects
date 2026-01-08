1d739a3320ad9c5a80d3219f11ed185ed84d6fb2
Rahul Singh <rahulsingh51775177@gmail.com>
2026-01-08 17:57:53 +0530

    Update 111_Extract Text from Images.py

--- Day_101/111_Extract Text from Images.py
+++ Day_101/111_Extract Text from Images.py
@@ -16,3 +16,18 @@ IMAGE_PATH = "sample.jpg"
 def preprocess(image_path):
     img = cv2.imread(image_path)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Noise removal
+    blur = cv2.GaussianBlur(gray, (5,5), 0)
+
+    # Thresholding
+    _, thresh = cv2.threshold(blur, 0, 255,
+                              cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    return thresh
+
+# -----------------------------
+# OCR FUNCTION
+# -----------------------------
+def extract_text(processed_img):
+    text = pytesseract.image_to_string(processed_img)
+    return text
