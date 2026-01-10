0660cba25135c24d2a8041a3747d8285af849682
Rahul Singh <rahulsingh51775177@gmail.com>
2026-01-10 22:30:49 +0530

    Update 113_Image Caption Generator.py

--- Day_101/113_Image Caption Generator.py
+++ Day_101/113_Image Caption Generator.py
@@ -13,3 +13,23 @@ cnn = tf.keras.applications.InceptionV3(
     include_top=False,
     pooling="avg"
 )
+# -----------------------------
+# IMAGE PREPROCESSING
+# -----------------------------
+def preprocess_image(path):
+    img = Image.open(path).resize((299, 299))
+    img = np.array(img)
+    img = tf.keras.applications.inception_v3.preprocess_input(img)
+    return np.expand_dims(img, axis=0)
+
+# -----------------------------
+# DUMMY VOCAB (for demo)
+# -----------------------------
+idx_to_word = {
+    0: "<start>",
+    1: "a",
+    2: "dog",
+    3: "on",
+    4: "grass",
+    5: "<end>"
+}
