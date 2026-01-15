2b219e5c3bdbf144e6f57c751dddc1b8544e2bcc
Rahul Singh <rahulsingh51775177@gmail.com>
2026-01-15 19:21:04 +0530

    Update 116_Self-Supervised Learning with SimCLR.py

--- Day_101/116_Self-Supervised Learning with SimCLR.py
+++ Day_101/116_Self-Supervised Learning with SimCLR.py
@@ -4,3 +4,12 @@
 import tensorflow as tf
 from tensorflow.keras import layers
 import numpy as np
+
+
+# -----------------------------
+# DATA AUGMENTATION
+# -----------------------------
+def augment(image):
+    image = tf.image.random_flip_left_right(image)
+    image = tf.image.random_brightness(image, 0.2)
+    image = tf.image.random_contrast(image, 0.8, 1.2)
