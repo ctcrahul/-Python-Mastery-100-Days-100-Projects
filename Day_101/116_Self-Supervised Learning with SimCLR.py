8e5907d1f4aacf44bf26bc9cb1597290df4b47f4
Rahul Singh <rahulsingh51775177@gmail.com>
2026-01-15 19:21:19 +0530

    Update 116_Self-Supervised Learning with SimCLR.py

--- Day_101/116_Self-Supervised Learning with SimCLR.py
+++ Day_101/116_Self-Supervised Learning with SimCLR.py
@@ -13,3 +13,12 @@ def augment(image):
     image = tf.image.random_flip_left_right(image)
     image = tf.image.random_brightness(image, 0.2)
     image = tf.image.random_contrast(image, 0.8, 1.2)
+    image = tf.image.resize(image, (224, 224))
+    return image
+
+# -----------------------------
+# CONTRASTIVE LOSS (NT-Xent)
+# -----------------------------
+def contrastive_loss(z1, z2, temperature=0.5):
+    z1 = tf.math.l2_normalize(z1, axis=1)
+    z2 = tf.math.l2_normalize(z2, axis=1)
