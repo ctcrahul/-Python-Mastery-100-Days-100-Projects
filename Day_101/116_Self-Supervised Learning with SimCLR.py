1e95c5a344e9d1c081f44b3bc98ac1fd768d797d
Rahul Singh <rahulsingh51775177@gmail.com>
2026-01-15 19:21:38 +0530

    Update 116_Self-Supervised Learning with SimCLR.py

--- Day_101/116_Self-Supervised Learning with SimCLR.py
+++ Day_101/116_Self-Supervised Learning with SimCLR.py
@@ -22,3 +22,28 @@ def augment(image):
 def contrastive_loss(z1, z2, temperature=0.5):
     z1 = tf.math.l2_normalize(z1, axis=1)
     z2 = tf.math.l2_normalize(z2, axis=1)
+  logits = tf.matmul(z1, z2, transpose_b=True) / temperature
+    labels = tf.range(tf.shape(z1)[0])
+    loss = tf.keras.losses.sparse_categorical_crossentropy(
+        labels, logits, from_logits=True
+    )
+    return tf.reduce_mean(loss)
+
+# -----------------------------
+# ENCODER NETWORK
+# -----------------------------
+def build_encoder():
+    base = tf.keras.applications.ResNet50(
+        include_top=False,
+        weights=None,
+        pooling="avg"
+    )
+    projection = tf.keras.Sequential([
+        layers.Dense(256, activation="relu"),
+        layers.Dense(128)
+    ])
+    return base, projection
+
+# -----------------------------
+# TRAINING LOOP
+# ---------------------------
