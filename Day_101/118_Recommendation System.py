b61aa5cb0b5308336328b8b88943e731824545dd
Rahul Singh <rahulsingh51775177@gmail.com>
2026-01-17 20:29:18 +0530

    Update 118_Recommendation System.py

--- Day_101/118_Recommendation System.py
+++ Day_101/118_Recommendation System.py
@@ -14,3 +14,14 @@ user_item = data.pivot_table(index="user_id", columns="item_id", values="rating"
 # -----------------------------
 # SIMILARITY MATRIX
 # -----------------------------
+similarity = cosine_similarity(user_item)
+similarity_df = pd.DataFrame(similarity, index=user_item.index, columns=user_item.index)
+
+# -----------------------------
+# RECOMMEND FUNCTION
+# -----------------------------
+def recommend(user_id, top_n=3):
+    similar_users = similarity_df[user_id].sort_values(ascending=False)[1:]
+    weighted_ratings = user_item.loc[similar_users.index].T.dot(similar_users)
+    recommendations = weighted_ratings.sort_values(ascending=False).head(top_n)
+    return recommendations.index.tolist()
