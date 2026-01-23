14af2068f3726198da104e5b7405bcbaf74126af
Rahul Singh <rahulsingh51775177@gmail.com>
2026-01-23 19:50:02 +0530

    Add convex hull drawing for decoded QR code objects

--- Day_101/124_Barcode and QR Code Scanner.py
+++ Day_101/124_Barcode and QR Code Scanner.py
@@ -10,3 +10,13 @@ def scan_codes(frame):
     for obj in decoded_objects:
         points = obj.polygon
         if len(points) > 4:
+            hull = cv2.convexHull(
+                np.array([point for point in points], dtype=np.float32)
+            )
+            hull = list(map(tuple, np.squeeze(hull)))
+        else:
+            hull = points
+
+        n = len(hull)
+        for j in range(n):
+            cv2.line(frame, hull[j], hull[(j + 1) % n], (0, 255, 0), 2)
