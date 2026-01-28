843efb96830df83cfed9c124406a8fdfa1c20dfc
Rahul Singh <rahulsingh51775177@gmail.com>
2026-01-28 20:05:06 +0530

    Create 129_Smart Energy Consumption Prediction.py

---
+++ Day_101/129_Smart Energy Consumption Prediction.py
@@ -0,0 +1,16 @@
+Smart Energy Consumption PredictionSmart Energy Consumption PredictionSmart Energy Consumption PredictionSmart Energy Consumption PredictionSmart Energy Consumption Predictionpip install numpy pandas scikit-learn
+
+pip install numpy pandas scikit-learn
+
+pip install numpy pandas scikit-learn
+
+pip install numpy pandas scikit-learn
+
+pip install numpy pandas scikit-learn
+
+
+pip install numpy pandas scikit-learn
+pip install numpy pandas scikit-learn
+
+
+pip install numpy pandas scikit-learn
