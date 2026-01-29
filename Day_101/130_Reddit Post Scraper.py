9b595284fc4f5fd4657f2d724eac8a1efad9b206
Rahul Singh <rahulsingh51775177@gmail.com>
2026-01-29 20:55:01 +0530

    Update 130_Reddit Post Scraper.py

--- Day_101/130_Reddit Post Scraper.py
+++ Day_101/130_Reddit Post Scraper.py
@@ -14,3 +14,27 @@ USER_AGENT = "reddit_scraper_by_your_username"
 # -----------------------------
 # CONNECT TO REDDIT
 # -----------------------------
+reddit = praw.Reddit(
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
+    user_agent=USER_AGENT
+)
+
+# -----------------------------
+# SCRAPER FUNCTION
+# -----------------------------
+def scrape_subreddit(subreddit_name, limit=50):
+    subreddit = reddit.subreddit(subreddit_name)
+
+    posts = []
+
+    for post in subreddit.hot(limit=limit):
+        posts.append({
+            "title": post.title,
+            "author": str(post.author),
+            "score": post.score,
+            "num_comments": post.num_comments,
+            "created_utc": post.created_utc,
+            "url": post.url,
+            "selftext": post.selftext
+        })
