f92a707f10a8ea128e1e74fd41fc46e0170a17d2
Rahul Singh <rahulsingh51775177@gmail.com>
2026-02-01 21:20:36 +0530

    Update 132_Titanic Survival Prediction.py

--- Day_101/132_Titanic Survival Prediction.py
+++ Day_101/132_Titanic Survival Prediction.py
@@ -12,3 +12,24 @@ from sklearn.metrics import accuracy_score, classification_report
 # LOAD DATA
 # -----------------------------
 df = pd.read_csv("train.csv")
+
+# -----------------------------
+# SELECT FEATURES
+# -----------------------------
+features = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
+target = "Survived"
+
+df = df[features + [target]]
+
+# -----------------------------
+# HANDLE MISSING VALUES
+# -----------------------------
+df["Age"].fillna(df["Age"].median(), inplace=True)
+df["Embarked"].fillna(df["Embarked"].mode()[0], inplace=True)
+
+# -----------------------------
+# ENCODE CATEGORICAL DATA
+# -----------------------------
+encoder = LabelEncoder()
+df["Sex"] = encoder.fit_transform(df["Sex"])
+df["Embarked"] = encoder.fit_transform(df["Embarked"])
