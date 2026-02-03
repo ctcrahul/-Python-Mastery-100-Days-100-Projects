b2907d327ef6b491b14c40745c38940bee8a9cf4
Rahul Singh <rahulsingh51775177@gmail.com>
2026-02-03 21:35:35 +0530

    Update 134_eye_control_laptop.py

--- Day_101/134_eye_control_laptop.py
+++ Day_101/134_eye_control_laptop.py
@@ -11,3 +11,27 @@ face_mesh = mp_face.FaceMesh(refine_landmarks=True)
 
 cam = cv2.VideoCapture(0)
 screen_w, screen_h = pyautogui.size()
+
+
+blink_threshold = 0.004
+blink_cooldown = 1
+last_blink_time = 0
+
+LEFT_EYE = [33, 133]
+RIGHT_EYE = [362, 263]
+IRIS = [474, 475, 476, 477]
+
+def eye_aspect_ratio(landmarks, eye):
+    left = landmarks[eye[0]]
+    right = landmarks[eye[1]]
+    return abs(left.y - right.y)
+
+while True:
+    ret, frame = cam.read()
+    if not ret:
+        break
+
+    frame = cv2.flip(frame, 1)
+    h, w, _ = frame.shape
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    result = face_mesh.process(rgb)
