e70e40ecefbf0921575878fd66177cf30b80814b
Rahul Singh <rahulsingh51775177@gmail.com>
2026-02-11 21:09:06 +0530

    Update Facial Expression Detection.py

--- last /Facial Expression Detection.py
+++ last /Facial Expression Detection.py
@@ -59,3 +59,68 @@ cap = cv2.VideoCapture(0)
 
 cap.release()
 cv2.destroyAllWindows()
+
+
+
+################# TRAIN MODEL
+
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+train_dir = "data/train"
+val_dir = "data/test"
+
+train_datagen = ImageDataGenerator(rescale=1./255)
+val_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(48,48),
+    batch_size=64,
+    color_mode="grayscale",
+    class_mode="categorical"
+)
+
+validation_generator = val_datagen.flow_from_directory(
+    val_dir,
+    target_size=(48,48),
+    batch_size=64,
+    color_mode="grayscale",
+    class_mode="categorical"
+)
+
+model = Sequential()
+
+model.add(Conv2D(32, kernel_size=(3,3), activation="relu", input_shape=(48,48,1)))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Conv2D(64, kernel_size=(3,3), activation="relu"))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Conv2D(128, kernel_size=(3,3), activation="relu"))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Flatten())
+
+model.add(Dense(128, activation="relu"))
+model.add(Dropout(0.5))
+
+model.add(Dense(7, activation="softmax"))
+
+model.compile(
+    loss="categorical_crossentropy",
+    optimizer=Adam(),
+    metrics=["accuracy"]
+)
+
+model.fit(
+    train_generator,
+    validation_data=validation_generator,
+    epochs=30
+)
+
+model.save("emotion_model.hdf5")
