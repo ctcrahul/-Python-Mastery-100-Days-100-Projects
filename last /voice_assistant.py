454a375caeabb7f57624609a41047347902ba6f4
Rahul Singh <rahulsingh51775177@gmail.com>
2026-02-13 20:23:30 +0530

    Update voice_assistant.py

--- last /voice_assistant.py
+++ last /voice_assistant.py
@@ -21,3 +21,30 @@ def take_command():
         print("Listening...")
         r.pause_threshold = 1
         audio = r.listen(source)
+
+    try:
+        command = r.recognize_google(audio).lower()
+        print("You:", command)
+    except:
+        return "none"
+
+    return command
+
+def tell_time():
+    time = datetime.datetime.now().strftime('%I:%M %p')
+    speak("Current time is " + time)
+
+def search_wiki(query):
+    speak("Searching Wikipedia")
+    result = wikipedia.summary(query, sentences=2)
+    speak(result)
+
+def open_youtube():
+    webbrowser.open("https://youtube.com")
+
+def open_google():
+    webbrowser.open("https://google.com")
+
+def play_song(song):
+    speak("Playing " + song)
+    pywhatkit.playonyt(song)
