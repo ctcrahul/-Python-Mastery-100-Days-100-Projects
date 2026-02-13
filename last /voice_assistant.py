7bb5e96c757ab62aebfe191ea677340cb1f6706e
Rahul Singh <rahulsingh51775177@gmail.com>
2026-02-13 20:22:15 +0530

    Create voice_assistant.py

---
+++ last /voice_assistant.py
@@ -0,0 +1,23 @@
+import speech_recognition as sr
+import pyttsx3
+import datetime
+import wikipedia
+import webbrowser
+import pywhatkit
+import os
+import requests
+
+engine = pyttsx3.init()
+engine.setProperty('rate', 170)
+
+def speak(text):
+    print("Assistant:", text)
+    engine.say(text)
+    engine.runAndWait()
+
+def take_command():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
