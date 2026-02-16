de4cefcf0e81bcb02f1dc4e6e3c2c50338e67957
Rahul Singh <rahulsingh51775177@gmail.com>
2026-02-16 21:01:57 +0530

    Update voice_assistant_nova.py

--- last /voice_assistant_nova.py
+++ last /voice_assistant_nova.py
@@ -16,3 +16,35 @@ from sklearn.linear_model import LogisticRegression
 
 engine = pyttsx3.init()
 engine.setProperty('rate', 170)
+
+def speak(text):
+    print("Assistant:", text)
+    engine.say(text)
+    engine.runAndWait()
+
+# -------- WAKE WORD --------
+
+q = queue.Queue()
+
+model = Model("vosk-model-small-en-us-0.15")
+rec = KaldiRecognizer(model, 16000)
+
+def callback(indata, frames, time, status):
+    q.put(bytes(indata))
+
+def listen_for_wake_word():
+    with sd.RawInputStream(samplerate=16000, blocksize=8000,
+                           dtype='int16', channels=1,
+                           callback=callback):
+
+        print("Waiting for wake word...")
+
+        while True:
+            data = q.get()
+            if rec.AcceptWaveform(data):
+                result = json.loads(rec.Result())
+                text = result.get("text", "")
+
+                if "hey nova" in text:
+                    speak("Yes?")
+                    return
