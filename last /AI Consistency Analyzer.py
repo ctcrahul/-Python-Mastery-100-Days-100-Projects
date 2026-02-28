aa70b7fe597726a3c9265802425ed9f2dec1d3cb
Rahul Singh <rahulsingh51775177@gmail.com>
2026-02-28 19:59:38 +0530

    Update AI Consistency Analyzer.py

--- last /AI Consistency Analyzer.py
+++ last /AI Consistency Analyzer.py
@@ -14,3 +14,27 @@ uploaded_file = st.file_uploader("Upload CSV File")
 if uploaded_file:
 
     df = pd.read_csv(uploaded_file)
+    st.subheader("Uploaded Data")
+    st.dataframe(df)
+
+    required_cols = ['study_hours','distractions','sleep','goals_completed']
+
+    if not all(col in df.columns for col in required_cols):
+        st.error("CSV must contain: study_hours, distractions, sleep, goals_completed")
+        st.stop()
+
+    scaler = MinMaxScaler()
+    scaled = scaler.fit_transform(df[required_cols])
+
+    df_scaled = pd.DataFrame(scaled, columns=required_cols)
+
+    # Consistency Score
+    df['Consistency Score'] = (
+        df_scaled['study_hours'] * 0.4 +
+        (1 - df_scaled['distractions']) * 0.3 +
+        df_scaled['sleep'] * 0.1 +
+        df_scaled['goals_completed'] * 0.2
+    ) * 100
+
+    # Effort Stability
+    df['Effort Stability'] = df['study_hours'].rolling(3).std().fillna(0)
